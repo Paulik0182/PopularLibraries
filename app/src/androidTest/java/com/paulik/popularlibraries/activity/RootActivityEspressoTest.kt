6eb2b82377abd1aaa1db709dcaa5303b9fa8c54e
Paul <paulik0182@gmail.com>
2023-06-18 14:59:22 +0300

    Тестирование активити и фрагментов с помощью Espresso

---
+++ app/src/androidTest/java/com/paulik/popularlibraries/activity/RootActivityEspressoTest.kt
@@ -0,0 +1,61 @@
+package com.paulik.popularlibraries.activity
+
+import androidx.lifecycle.Lifecycle
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.paulik.popularlibraries.R
+import com.paulik.popularlibraries.ui.root.RootActivity
+import junit.framework.TestCase
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class RootActivityEspressoTest {
+
+    private lateinit var scenario: ActivityScenario<RootActivity>
+
+    @Before
+    fun setup() {
+        scenario = ActivityScenario.launch(RootActivity::class.java)
+    }
+
+    @Test
+    fun activity_AssertNotNull() {
+        scenario.onActivity {
+            TestCase.assertNotNull(it)
+        }
+    }
+
+    @Test
+    fun activity_IsResumed() {
+        TestCase.assertEquals(Lifecycle.State.RESUMED, scenario.state)
+    }
+
+    @Test
+    fun testBottomNavigationBar() {
+
+        // Нажимаем на кнопку "Настройки" в нижней панели приложения
+        onView(withId(R.id.settings_item)).perform(click())
+
+        // Проверяем, что фрагмент настроек был отображен на экране
+        onView(withId(R.id.fragment_settings)).check(matches(isDisplayed()))
+
+        // Нажимаем на кнопку "стартовой страницы" в нижней панели приложения
+        onView(withId(R.id.starting_item)).perform(click())
+
+        // Проверяем, что фрагмент стартовой страницы был отображен на экране
+        onView(withId(R.id.fragment_starting)).check(matches(isDisplayed()))
+    }
+
+    @After
+    fun close() {
+        scenario.close()
+    }
+}
---
+++ app/src/androidTest/java/com/paulik/popularlibraries/fragment/StartingFragmentEspressoTest.kt
@@ -0,0 +1,87 @@
+package com.paulik.popularlibraries.fragment
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.paulik.popularlibraries.R
+import com.paulik.popularlibraries.ui.root.RootActivity
+import com.paulik.popularlibraries.ui.root.StartingFragment
+import junit.framework.TestCase.assertNotNull
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class StartingFragmentEspressoTest {
+
+    private lateinit var activityScenario: ActivityScenario<RootActivity>
+    private lateinit var fragment: StartingFragment
+
+    @Before
+    fun setup() {
+        // создали активити на которой будет раздут фрагмент
+        activityScenario = ActivityScenario.launch(RootActivity::class.java)
+
+        // Проверяем, что фрагмент стартовой страницы был отображен на экране
+        onView(withId(R.id.fragment_starting)).check(matches(ViewMatchers.isDisplayed()))
+
+        //создали фрагмент (нашли фрагмент)
+        activityScenario.onActivity { activity ->
+            fragment = StartingFragment()
+            activity.supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container_frame_layout, fragment, null)
+                .commitNow()
+        }
+    }
+
+    @Test
+    fun testFragmentNotNull() {
+        assertNotNull(fragment)
+    }
+
+    @Test
+    fun fragmentButtons_AreEffectiveVisible() {
+        onView(withId(R.id.counter_mvp_button))
+            .check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))
+        onView(withId(R.id.users_git_hub_button))
+            .check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))
+        onView(withId(R.id.counter_mvp_button))
+            .check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))
+    }
+
+    @Test
+    fun counterButton_IsWorking() {
+        // Нажимаем на кнопку "счетчика"
+        onView(withId(R.id.counter_mvp_button)).perform(click())
+        // Проверяем, что фрагмент счетчика был отображен на экране
+        onView(withId(R.id.fragment_counter)).check(matches(ViewMatchers.isDisplayed()))
+
+    }
+
+    @Test
+    fun userButton_IsWorking() {
+        // Нажимаем на кнопку "пользователя gitHub"
+        onView(withId(R.id.users_git_hub_button)).perform(click())
+        // Проверяем, что фрагмент пользователя gitHub был отображен на экране
+        onView(withId(R.id.fragment_users_git_hub)).check(matches(ViewMatchers.isDisplayed()))
+    }
+
+    @Test
+    fun convertorButton_IsWorking() {
+        // Нажимаем на кнопку "конвертора изображения"
+        onView(withId(R.id.convertor_image_button)).perform(click())
+        // Проверяем, что фрагмент конвертора был отображен на экране
+        onView(withId(R.id.fragment_convertor)).check(matches(ViewMatchers.isDisplayed()))
+    }
+
+    @After
+    fun close() {
+        activityScenario.close()
+    }
+}
